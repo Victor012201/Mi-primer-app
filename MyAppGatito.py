49b0fb04ad0a6b52fea111829e9e9ee3d542c4b4
Victor012201 <64226340+Victor012201@users.noreply.github.com>
2022-04-01 14:18:47 -0700

    Update MyAppGatito.py

--- MyAppGatito.py
+++ MyAppGatito.py
@@ -2,3 +2,5 @@ import streamlit as st
 st.title("Mi primer app")
 st.button("SI")
 st.button("NO")
+import pandas as pd
+df = pd.read_csv("https://raw.githubusercontent.com/quantum-apps/map/main/data.csv")
