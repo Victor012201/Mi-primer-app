9ecc89030b0aa6206c84428e001fe593541f7746
Victor012201 <64226340+Victor012201@users.noreply.github.com>
2022-04-01 14:32:18 -0700

    .

--- MyAppGatito.py
+++ MyAppGatito.py
@@ -1,8 +1,8 @@
 import streamlit as st
 st.title("Mi primer app")
-st.button("SI")
-st.button("NO")
-import pandas as pd
-df = pd.read_csv("https://raw.githubusercontent.com/quantum-apps/mapa/main/data.csv")
-st.write(df)
-st.map(df)
+#st.button("SI")
+#st.button("NO")
+#import pandas as pd
+#df = pd.read_csv("https://raw.githubusercontent.com/quantum-apps/mapa/main/data.csv")
+#st.write(df)
+#st.map(df)
