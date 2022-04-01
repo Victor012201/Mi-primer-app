6fea641ff5c0ac975f4c22307b83423e07e7ab2d
Victor012201 <64226340+Victor012201@users.noreply.github.com>
2022-04-01 15:07:27 -0700

    1

--- MyAppGatito.py
+++ MyAppGatito.py
@@ -10,8 +10,8 @@ num2=st.slider("elige el numero 2",0.0,100.0,25.0)
 suma=num1+num2
 st.write("la suma de",num1,"y",num2,"es :",suma)
 st.write("ahora multipliquemos")
-nm1=st.number_imput("Dame n1")
-nm2=st.number_imput("Dame n2")
+nm1=st.number_input("Dame n1")
+nm2=st.number_input("Dame n2")
 mult=nn1*nn2
 st.write("la multiplicacion de",nn1,"y",nn2,"es :",mult)
 #import pandas as pd
