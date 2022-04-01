afcd8d8f86510da029c36fdc5949a79a6b3e7317
Victor012201 <64226340+Victor012201@users.noreply.github.com>
2022-04-01 15:10:41 -0700

    .

--- MyAppGatito.py
+++ MyAppGatito.py
@@ -9,7 +9,7 @@ num1=st.slider("elige el numero 1",0.0,100.0,25.0)
 num2=st.slider("elige el numero 2",0.0,100.0,25.0)
 suma=num1+num2
 st.write("la suma de",num1,"y",num2,"es :",suma)
-st.write("ahora multipliquemos",nm1,"por",nm2,"es :", mult)
+st.write("ahora multipliquemos")
 with st.sidebar:
     nm1=st.number_input("Dame n1")
     nm2=st.number_input("Dame n2")
